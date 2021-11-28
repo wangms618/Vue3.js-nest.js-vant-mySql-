cf88fab700ad713be4813b962658f684c6070848
王铭顺 <553147551@qq.com>
2021-11-28 16:54:52 +0800

    操作数据库

--- forum-rearend/src/app.module.ts
+++ forum-rearend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from './posts/posts.module';
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
