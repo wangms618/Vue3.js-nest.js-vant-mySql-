77254e6911163a6b77999d8f1fe5388ac31e5c84
553147551 <553147551@qq.com>
2021-11-29 17:42:31 +0800

    数据库设计1

--- forum-rearend/src/posts/posts.entity.ts
+++ forum-rearend/src/posts/posts.entity.ts
@@ -1,8 +1,10 @@
 //    posts/posts.entity.ts
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+// @Entity 装饰器作用: 告诉typeorm，PostsEntity是一个实体，这个实体是映射到数据库的一个表格
 @Entity("posts")
 export class PostsEntity {
+    // @PrimaryGeneratedColumn装饰器作用：在用户表里面创建一个id字段，并将其设置为主键 ，并自增
     @PrimaryGeneratedColumn()
     id:number; // 标记为主列，值自动生成
 
