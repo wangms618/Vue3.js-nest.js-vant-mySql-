1861823956f5433947f2b5674bbb095c6740f60e
王铭顺 <553147551@qq.com>
2022-04-01 23:03:53 +0800

    编辑框

--- forum-frontend/src/views/edit-posts/components/edit-area.vue
+++ forum-frontend/src/views/edit-posts/components/edit-area.vue
@@ -1,14 +1,36 @@
 <template>
-    <div class="edit-area">编辑区</div>
+    <div class="edit-area">
+        <van-field
+            type="textarea"
+            v-model="value"
+            placeholder="在这里书写文章"
+            :maxlength="240"
+            show-word-limit
+        />
+    </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const value = ref("");
+        return { value };
+    },
+};
 </script>
 
 <style lang="less" scoped>
 .edit-area {
     width: 100vw;
-    height: 30vh;
+    height: 24vh;
+    /deep/.van-field__body {
+        height: 20vh;
+        textarea{
+            height: 100%;
+            font-size: 16px;
+        }
+    }
 }
 </style>
