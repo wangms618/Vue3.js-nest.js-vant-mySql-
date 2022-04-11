f8e4eb4b562461d86d36cbd8212440df5e20e0c9
王铭顺 <553147551@qq.com>
2022-04-11 16:36:18 +0800

    信息卡片

--- forum-frontend/src/views/homepage/components/user-card.vue
+++ forum-frontend/src/views/homepage/components/user-card.vue
@@ -2,12 +2,24 @@
     <div class="card">
         <div class="card-header">
             <div class="card-header__img">
-                
+                <van-image
+                    round
+                    width="68"
+                    height="68"
+                    src="https://cdn.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+                />
             </div>
-            <div class="card-header__name"></div>
+            <div class="card-header__name">
+                <span>来自外星的怪兽</span>
+            </div>
         </div>
-        <div class="card-content"></div>
-        <div class="card-footed"></div>
+        <div class="card-content">
+            <span>个性签名等等等等</span>
+        </div>
+        <div class="card-footer">
+            <div class="card-footer__concerned">关注 0</div>
+            <div class="card-footer__fans">粉丝 4</div>
+        </div>
     </div>
 </template>
 
@@ -22,9 +34,31 @@ export default {};
     background-color: #25c689;
     box-sizing: border-box;
     padding: 20px;
+    line-height: 20px;
+    color: #fff;
     &-header {
         width: 100%;
         height: 80px;
+        box-sizing: border-box;
+        padding: 6px;
+        display: flex;
+        &__name {
+            margin-left: 20px;
+            font-size: 20px;
+            display: flex;
+            align-items: center;
+        }
+    }
+    &-content {
+        font-size: 16px;
+        margin: 20px 0 16px 10px;
+    }
+    &-footer {
+        margin-left: 10px;
+        font-size: 18px;
+        display: flex;
+        width: 120px;
+        justify-content: space-between;
     }
 }
 </style>
