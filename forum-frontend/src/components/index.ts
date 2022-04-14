e5c62ca2ae8d0017c138b5c99d777aef1070c095
王铭顺 <553147551@qq.com>
2022-04-14 18:45:06 +0800

    更新

--- forum-frontend/src/components/index.ts
+++ forum-frontend/src/components/index.ts
@@ -1,5 +1,6 @@
-import EmptyTip from "./empty-tip.vue"
-import PostsList from "./posts-list.vue"
-import SearchBar from "./search-bar.vue"
-
-export { EmptyTip, PostsList, SearchBar }
+import EmptyTip from "./empty-tip.vue";
+import PostsList from "./posts-list.vue";
+import SearchBar from "./search-bar.vue";
+import AddPictures from "./add-pictures.vue";
+import DynamicItem from "./dynamic-item.vue";
+export { EmptyTip, PostsList, SearchBar, AddPictures, DynamicItem };
